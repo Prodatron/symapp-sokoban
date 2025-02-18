200e5fbed940d8008c119864c04c4b7e907e6215
Prodatron <jmika@prodatron.net>
2025-02-18 13:30:20 +0100

    makefile updated

--- App-Sokoban-Head.asm
+++ App-Sokoban-Head.asm
@@ -3,7 +3,7 @@ nolist
 org #1000
 
 WRITE "e:\symbos\apps\sokoban.exe"
-READ "..\..\..\..\SVN-Main\trunk\SymbOS-Constants.asm"
+READ "..\..\..\SRC-Main\SymbOS-Constants.asm"
 
 relocate_start
 
@@ -135,9 +135,9 @@ use_SyFile_DIRMOV       equ 0   ;Moves a file or sub directory
 use_SyFile_DIRINF       equ 0   ;Returns information about one drive
 use_SyFile_DEVDIR       equ 0   ;Reads the content of a directory (extended)
 
-READ "..\..\..\..\SVN-Main\trunk\Docs-Developer\symbos_lib-SystemManager.asm"
-READ "..\..\..\..\SVN-Main\trunk\Docs-Developer\symbos_lib-DesktopManager.asm"
-READ "..\..\..\..\SVN-Main\trunk\Docs-Developer\symbos_lib-FileManager.asm"
+READ "..\..\..\SRC-Main\Docs-Developer\symbos_lib-SystemManager.asm"
+READ "..\..\..\SRC-Main\Docs-Developer\symbos_lib-DesktopManager.asm"
+READ "..\..\..\SRC-Main\Docs-Developer\symbos_lib-FileManager.asm"
 READ "App-Sokoban.asm"
 
 App_EndTrns
